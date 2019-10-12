8f974260e70493c01e675d01a23665670c81f562
Lucas Canto <lucas.canto@siemens.com>
2019-10-12 10:30:16 -0300

    Exporting final datasets for VALE3

--- preprocessor/main.py
+++ preprocessor/main.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
+import csv
+import os
 from datetime import timedelta
+
+OUT_DIR = './dataset_out'
 
 df_companies = pd.read_csv('../datasets/company-codes.csv')
 df_bovespa = pd.read_csv('../datasets/kaggle/bovespa.csv')
@@ -20,6 +24,12 @@ def getEffectDate(date):
         effectDate = effectDate + timedelta(days=1)
     return effectDate
 
+def exportToTSV(dataframe, filename):
+    if not os.path.exists(OUT_DIR):
+        os.mkdir(OUT_DIR)
+    fullpath = '%s/%s' % (OUT_DIR, filename)
+    dataframe.to_csv(fullpath, sep='\t', quoting=csv.QUOTE_NONE, index=False)
+
 df_articles = df_articles[df_articles.category == 'mercado']
 artigos_unused_columns = ['text', 'category', 'subcategory', 'link']
 df_articles.drop(artigos_unused_columns, inplace=True, axis=1)
@@ -37,6 +47,7 @@ df_bovespa = df_bovespa.sort_values('date')
 df_articles['date'] = df_articles.apply(lambda row: getEffectDate(row.date), axis=1)
 df_articles = df_articles.sort_values('date')
 
+# TODO: fazer loop para varrer empresas da lista e avancos de dias (1~5)
 # VALE3
 df_bovespa_vale3 = df_bovespa[df_bovespa.codneg.str.strip() == 'VALE3']
 df_bovespa_vale3 = df_bovespa_vale3.assign(close1d=df_bovespa_vale3['close'].transform( lambda group: group.shift(-1)))
@@ -46,3 +57,25 @@ df_bovespa_vale3 = df_bovespa_vale3.assign(close4d=df_bovespa_vale3['close'].tra
 df_bovespa_vale3 = df_bovespa_vale3.assign(close5d=df_bovespa_vale3['close'].transform( lambda group: group.shift(-5)))
 df_bovespa_vale3.drop('close', inplace=True, axis=1)
 df_vale3 = pd.merge(df_articles, df_bovespa_vale3, on='date', how='left')
+
+df_vale3_1d = df_vale3.drop(['date', 'codneg', 'close2d', 'close3d', 'close4d', 'close5d'], axis=1)
+df_vale3_1d['label'] = df_vale3_1d.apply(lambda row: isBigger(row['open'], row['close1d']), axis=1)
+df_vale3_1d = df_vale3_1d.drop(['open', 'close1d'], axis=1)
+df_vale3_2d = df_vale3.drop(['date', 'codneg', 'close1d', 'close3d', 'close4d', 'close5d'], axis=1)
+df_vale3_2d['label'] = df_vale3_2d.apply(lambda row: isBigger(row['open'], row['close2d']), axis=1)
+df_vale3_2d = df_vale3_2d.drop(['open', 'close2d'], axis=1)
+df_vale3_3d = df_vale3.drop(['date', 'codneg', 'close1d', 'close2d', 'close4d', 'close5d'], axis=1)
+df_vale3_3d['label'] = df_vale3_3d.apply(lambda row: isBigger(row['open'], row['close3d']), axis=1)
+df_vale3_3d = df_vale3_3d.drop(['open', 'close3d'], axis=1)
+df_vale3_4d = df_vale3.drop(['date', 'codneg', 'close1d', 'close2d', 'close3d', 'close5d'], axis=1)
+df_vale3_4d['label'] = df_vale3_4d.apply(lambda row: isBigger(row['open'], row['close4d']), axis=1)
+df_vale3_4d = df_vale3_4d.drop(['open', 'close4d'], axis=1)
+df_vale3_5d = df_vale3.drop(['date', 'codneg', 'close1d', 'close2d', 'close3d', 'close4d'], axis=1)
+df_vale3_5d['label'] = df_vale3_5d.apply(lambda row: isBigger(row['open'], row['close5d']), axis=1)
+df_vale3_5d = df_vale3_5d.drop(['open', 'close5d'], axis=1)
+
+exportToTSV(df_vale3_1d, 'vale3_1d.tsv')
+exportToTSV(df_vale3_2d, 'vale3_2d.tsv')
+exportToTSV(df_vale3_3d, 'vale3_3d.tsv')
+exportToTSV(df_vale3_4d, 'vale3_4d.tsv')
+exportToTSV(df_vale3_5d, 'vale3_5d.tsv')
