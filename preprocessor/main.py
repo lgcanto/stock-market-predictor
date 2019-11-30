69828c6a4a059e6435262ed621f79045c7b4480f
Lucas Canto <lucas.canto@siemens.com>
2019-11-30 15:10:23 -0300

    Adding MAX_DAYS parameter in main.py and iterating those days for building datasets for custom time wnidows; Removing duplicates from companies.csv

--- preprocessor/main.py
+++ preprocessor/main.py
@@ -5,6 +5,7 @@ import os
 from datetime import timedelta
 
 OUT_DIR = './dataset_out'
+MAX_DAYS = 5
 
 df_companies = pd.read_csv('../datasets/company-codes.csv')
 df_bovespa = pd.read_csv('../datasets/kaggle/bovespa.csv')
@@ -47,67 +48,23 @@ df_bovespa = df_bovespa.sort_values('date')
 df_articles['date'] = df_articles.apply(lambda row: getEffectDate(row.date), axis=1)
 df_articles = df_articles.sort_values('date')
 
+df_analysis = pd.DataFrame(columns=['dataset','1s','0s', '-1s'])
+
 for index, row in df_companies.iterrows():
     print('Generating for ' + row['code'])
-    df_bovespa_company = df_bovespa[df_bovespa.codneg.str.strip() == row['code']]
-    df_bovespa_company = df_bovespa_company.assign(close0=df_bovespa_company['close'].transform(lambda group: group.shift(1)))
-    df_bovespa_company = df_bovespa_company.assign(close1d=df_bovespa_company['close'].transform(lambda group: group.shift(-1)))
-    df_bovespa_company = df_bovespa_company.assign(close2d=df_bovespa_company['close'].transform(lambda group: group.shift(-2)))
-    df_bovespa_company = df_bovespa_company.assign(close3d=df_bovespa_company['close'].transform(lambda group: group.shift(-3)))
-    df_bovespa_company = df_bovespa_company.assign(close4d=df_bovespa_company['close'].transform(lambda group: group.shift(-4)))
-    df_bovespa_company = df_bovespa_company.assign(close5d=df_bovespa_company['close'].transform(lambda group: group.shift(-5)))
-    df_bovespa_company = df_bovespa_company[~np.isnan(df_bovespa_company.close0)]
-    df_bovespa_company.drop('close', inplace=True, axis=1)
-    df_company = pd.merge(df_articles, df_bovespa_company, on='date', how='inner')
-
-    if df_company.size > 0:
-        print(' 1 day window:')
-        df_company_1d = df_company.drop(['date', 'codneg', 'close2d', 'close3d', 'close4d', 'close5d'], axis=1)
-        df_company_1d = df_company_1d[~np.isnan(df_company_1d.close1d)]
-        df_company_1d['label'] = df_company_1d.apply(lambda row: getAppreciation(row['close0'], row['close1d']), axis=1)
-        df_company_1d = df_company_1d.drop(['close0', 'close1d'], axis=1)
-        print('  number of positive registers: ' + str(df_company_1d[df_company_1d.label == 1].size))
-        print('  number of neutral registers: ' + str(df_company_1d[df_company_1d.label == 0].size))
-        print('  number of negative registers: ' + str(df_company_1d[df_company_1d.label == -1].size))
-        
-        print(' 2 days window:')
-        df_company_2d = df_company.drop(['date', 'codneg', 'close1d', 'close3d', 'close4d', 'close5d'], axis=1)
-        df_company_2d = df_company_2d[~np.isnan(df_company_2d.close2d)]
-        df_company_2d['label'] = df_company_2d.apply(lambda row: getAppreciation(row['close0'], row['close2d']), axis=1)
-        df_company_2d = df_company_2d.drop(['close0', 'close2d'], axis=1)
-        print('  number of positive registers: ' + str(df_company_2d[df_company_2d.label == 1].size))
-        print('  number of neutral registers: ' + str(df_company_2d[df_company_2d.label == 0].size))
-        print('  number of negative registers: ' + str(df_company_2d[df_company_2d.label == -1].size))
-        
-        print(' 3 days window:')
-        df_company_3d = df_company.drop(['date', 'codneg', 'close1d', 'close2d', 'close4d', 'close5d'], axis=1)
-        df_company_3d = df_company_3d[~np.isnan(df_company_3d.close3d)]
-        df_company_3d['label'] = df_company_3d.apply(lambda row: getAppreciation(row['close0'], row['close3d']), axis=1)
-        df_company_3d = df_company_3d.drop(['close0', 'close3d'], axis=1)
-        print('  number of positive registers: ' + str(df_company_3d[df_company_3d.label == 1].size))
-        print('  number of neutral registers: ' + str(df_company_3d[df_company_3d.label == 0].size))
-        print('  number of negative registers: ' + str(df_company_3d[df_company_3d.label == -1].size))
-        
-        print(' 4 days window:')
-        df_company_4d = df_company.drop(['date', 'codneg', 'close1d', 'close2d', 'close3d', 'close5d'], axis=1)
-        df_company_4d = df_company_4d[~np.isnan(df_company_4d.close4d)]
-        df_company_4d['label'] = df_company_4d.apply(lambda row: getAppreciation(row['close0'], row['close4d']), axis=1)
-        df_company_4d = df_company_4d.drop(['close0', 'close4d'], axis=1)
-        print('  number of positive registers: ' + str(df_company_4d[df_company_4d.label == 1].size))
-        print('  number of neutral registers: ' + str(df_company_4d[df_company_4d.label == 0].size))
-        print('  number of negative registers: ' + str(df_company_4d[df_company_4d.label == -1].size))
-        
-        print(' 5 days window:')
-        df_company_5d = df_company.drop(['date', 'codneg', 'close1d', 'close2d', 'close3d', 'close4d'], axis=1)
-        df_company_5d = df_company_5d[~np.isnan(df_company_5d.close5d)]
-        df_company_5d['label'] = df_company_5d.apply(lambda row: getAppreciation(row['close0'], row['close5d']), axis=1)
-        df_company_5d = df_company_5d.drop(['close0', 'close5d'], axis=1)
-        print('  number of positive registers: ' + str(df_company_5d[df_company_5d.label == 1].size))
-        print('  number of neutral registers: ' + str(df_company_5d[df_company_5d.label == 0].size))
-        print('  number of negative registers: ' + str(df_company_5d[df_company_5d.label == -1].size))
-        
-        exportToTSV(df_company_1d, row['code'] + '_1d.tsv')
-        exportToTSV(df_company_2d, row['code'] + '_2d.tsv')
-        exportToTSV(df_company_3d, row['code'] + '_3d.tsv')
-        exportToTSV(df_company_4d, row['code'] + '_4d.tsv')
-        exportToTSV(df_company_5d, row['code'] + '_5d.tsv')
+    df_full = df_bovespa[df_bovespa.codneg.str.strip() == row['code']]
+    df_full = df_full.assign(close_before=df_full['close'].transform(lambda group: group.shift(1)))
+    df_full = df_full[~np.isnan(df_full.close_before)]
+    for d in range(MAX_DAYS):
+        interval = d + 1
+        df_interval = df_full.assign(close_after=df_full['close'].transform(lambda group: group.shift(-interval)))
+        df_interval = df_interval[~np.isnan(df_interval.close_after)]
+        df_interval.drop('close', inplace=True, axis=1)
+        df_company = pd.merge(df_articles, df_interval, on='date', how='inner')
+        if df_company.size > 0:
+            df_company.drop(['date', 'codneg'], inplace=True, axis=1)
+            df_company['label'] = df_company.apply(lambda row: getAppreciation(row['close_before'], row['close_after']), axis=1)
+            df_company.drop(['close_before', 'close_after'], inplace=True, axis=1)
+            analysis = pd.Series({"dataset": row['code'] + '_' + str(interval) + 'd.tsv', "1s": df_company[df_company.label == 1].size, "0s": df_company[df_company.label == 0].size, "-1s": df_company[df_company.label == -1].size})
+            df_analysis = df_analysis.append(analysis, ignore_index=True)
+            exportToTSV(df_company, row['code'] + '_' + str(interval) + 'd.tsv')
